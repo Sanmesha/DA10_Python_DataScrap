6cc482319664b86ec76d2d86a1410b22567a793f
Sanmesha <sanmeshabhagat16@gmail.com>
2024-12-16 17:01:28 +0530

    this is scrapy task (Books)

--- Books/Books/spiders/bookspage.py
+++ Books/Books/spiders/bookspage.py
@@ -7,14 +7,14 @@ class BookspageSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        # Extract all book titles on the current page
+        
         books = response.css("article.product_pod h3 a::attr(title)").getall()
         
-        # Save book titles as rows in the output CSV
+       
         for book in books:
             yield {"title": book}
 
-        # Find the link to the next page and follow it if it exists
+        
         next_page = response.css("ul.pager li.next a::attr(href)").get()
         if next_page:
             next_page_url = response.urljoin(next_page)
